6f6353c7c66659b7032daff8f21b658769c90136
Ponti-D <spaediaz@gmail.com>
2023-02-08 21:49:59 +0000

    added more tests for speciesname and planetname

--- src/components/planet_name.test.tsx
+++ src/components/planet_name.test.tsx
@@ -1,8 +1,23 @@
 import "@testing-library/jest-dom";
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import PlanetName from "./planet_name";
 
-test("renders form element", async () => {
+test("renders Planet Name label element", async () => {
   render(<PlanetName value="" onChangePlanetName={() => {}} />);
   expect(screen.getByText("Planet Name:")).toBeInTheDocument();
+});
+test('displays planet Name element value as expected', () => {  
+    const onChangeHandler = jest.fn();
+    render(<PlanetName value='Mars' onChangePlanetName={onChangeHandler}/>);
+    const planetNameElement = screen.getByRole('textbox');    
+    expect(planetNameElement).toHaveValue('Mars');
+  });
+
+test("Check if funciton is being called as expected", async () => { 
+  const onChangeHandler = jest.fn();
+  render(<PlanetName value="" onChangePlanetName={onChangeHandler} />);
+
+  await userEvent.type(screen.getByRole("textbox"), "Mars");
+  expect(onChangeHandler).toHaveBeenCalledTimes(4);
 });
--- src/components/species_name.test.tsx
+++ src/components/species_name.test.tsx
@@ -1,8 +1,24 @@
 import "@testing-library/jest-dom";
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SpeciesName from "./species_name";
 
-test("renders form element", async () => {
+test("renders label text element", async () => {
   render(<SpeciesName value="" onChangeSpeciesName={() => {}} />);
   expect(screen.getByText("Species Name:")).toBeInTheDocument();
+});
+
+test('displays speciesName value as expected', () => {  
+    const onChangeHandler = jest.fn();
+    render(<SpeciesName value='Humans' onChangeSpeciesName={onChangeHandler}/>);
+    const speciesNameElement = screen.getByRole('textbox');    
+    expect(speciesNameElement).toHaveValue('Humans');
+  });
+
+test("Check if funciton is being called as expected", async () => { 
+  const onChangeHandler = jest.fn();
+  render(<SpeciesName value="" onChangeSpeciesName={onChangeHandler} />);
+
+  await userEvent.type(screen.getByRole("textbox"), "Humans");
+  expect(onChangeHandler).toHaveBeenCalledTimes(6);
 });
