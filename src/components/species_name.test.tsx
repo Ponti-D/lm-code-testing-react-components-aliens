afcc1180e6108729f51da31432b4c1fda6c7b4f7
Ponti-D <spaediaz@gmail.com>
2023-02-10 18:29:34 +0000

    added Species Name test for validation

--- src/components/species_name.test.tsx
+++ src/components/species_name.test.tsx
@@ -3,23 +3,28 @@ import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SpeciesName from "./species_name";
 
-test('renders label text element', () => {
+test("renders label text element", () => {
   render(<SpeciesName value="" onChangeSpeciesName={() => {}} />);
   expect(screen.getByText("Species Name:")).toBeInTheDocument();
 });
 
-test('displays speciesName value as expected', () => {  
-    const onChangeHandler = jest.fn();
-    render(<SpeciesName value='Humans' onChangeSpeciesName={onChangeHandler}/>);
-    
-    const speciesNameElement = screen.getByRole('textbox');    
-    expect(speciesNameElement).toHaveValue('Humans');
-  });
-
-test('Check if funciton is being called as expected', async () => { 
+test("displays speciesName value as expected", () => {
   const onChangeHandler = jest.fn();
-  render(<SpeciesName value='' onChangeSpeciesName={onChangeHandler} />);
+  render(<SpeciesName value="Humans" onChangeSpeciesName={onChangeHandler} />);
 
-  await userEvent.type(screen.getByRole('textbox'), 'Humans');
+  const speciesNameElement = screen.getByRole("textbox");
+  expect(speciesNameElement).toHaveValue("Humans");
+});
+
+test("Check if funciton is being called as expected", async () => {
+  const onChangeHandler = jest.fn();
+  render(<SpeciesName value="" onChangeSpeciesName={onChangeHandler} />);
+
+  await userEvent.type(screen.getByRole("textbox"), "Humans");
   expect(onChangeHandler).toHaveBeenCalledTimes(6);
+});
+
+it('Given the speciesName entered correctly there should not be error messages present', () => {
+  render(<SpeciesName value="Humans" onChangeSpeciesName={() => {}} />);
+  expect(screen.queryByText(/error:/i)).not.toBeInTheDocument();
 });
